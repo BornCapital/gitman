faaf8351392ca10fe8ef0a956a00e77d1a93c1b8
Jonathan Brannan <jbrannan@borncapital.com>
2014-09-08 08:43:57 -0500

    Adding errors when package does not exist refs #1117

--- Gitman/shell.py
+++ Gitman/shell.py
@@ -1,7 +1,29 @@
 #!/usr/bin/env python
 import sys
 sys.path.insert(0,'/usr/share/yum-cli/')
+import cli
 import shell
+import yummain
+import yum
+exit_code = 0
+old_install = cli.YumBaseCli.install
+def install(*args, **kwargs):
+  global exit_code
+  try:
+    old_install(*args, **kwargs)
+  except yum.Errors.InstallError:
+    exit_code = 1
+    raise
+cli.YumBaseCli.install = install
+
+old_exit = sys.exit
+def exit(code=0):
+  if code == 0:
+    old_exit(exit_code)
+  else:
+    old_exit(code)
+sys.exit = exit
+
 old_run = shell.YumShell.do_run
 def do_run(self, line):
   ret = old_run(self, line)
@@ -10,7 +32,6 @@ def do_run(self, line):
   return ret
 shell.YumShell.do_run = do_run
 try:
-  import yummain
   yummain.user_main(sys.argv[1:], exit_code=True)
 except KeyboardInterrupt, e:
     print >> sys.stderr, "\n\nExiting on user cancel."
